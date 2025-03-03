39134610b2f463e57d14b8a3e81702f651d30220
Dinithi1625403 <dinithi1625403@gmail.com>
2025-03-03 21:53:41 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Books Management System</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        
+        <h1>Welcome to the Books Management System</h1>
+        <p><a href="add_book.php">Add New Book</a></p>
+        <p><a href="view_books.php">View Books</a></p>
+        <p><a href="logout.php">Logout</a></p>
+    </div>
+</body>
+</html>
