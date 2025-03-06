f6a236f4205016bf6023e7f2543341232f7a109e
Dinithi1625403 <dinithi1625403@gmail.com>
2025-03-06 20:06:32 +0530

    added book number file

---
+++ bookNumber.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<form, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <form method="post" action="">
+    <fieldset>
+        <legend>Add new book</legend>
+        <table>
+            <tr>
+                <td>Book Number:</td>
+                <td><input type="number" maxlength="4" name="book-index"/></td>
+                <td><input type="submit" value="submit" name="sub"/></td>
+                <a href="">Back to List</a>
+            </tr>
+            
+              
+    </fieldset>
+    </form>
+</body>
+</html>
