b1caa84199a317d64743bd4ed7200d8f2d84538f
Dinithi1625403 <dinithi1625403@gmail.com>
2025-03-07 20:20:05 +0530

    connected with mysql

--- bookNumber.php
+++ bookNumber.php
@@ -14,7 +14,7 @@
         <table>
             <tr>
                 <td>Book Number:</td>
-                <td><input type="number" maxlength="4" name="book-index"/></td>
+                <td><input type="number" maxlength="4" name="book_index"/></td>
                 <td><input type="submit" value="submit" name="sub"/></td>
                 <a href="">Back to List</a>
             </tr>
@@ -24,3 +24,24 @@
     </form>
 </body>
 </html>
+
+<?php
+     if(isset($_POST['sub'])){
+        include "connection.php";
+        if(!isset($con)||!$con){
+            die("Database connection not established! Check connection.php.");
+
+        }
+        $book_index = mysqli_real_escape_string($con, $_POST['book_index']);
+        include "connection.php";
+        $sql_select="SELECT * FROM book WHERE `book_index`='$book_index'";
+        $result=mysqli_query($con,$sql_select);
+        if(mysqli_num_rows($result)>0){
+            echo "The book data is available in the database";
+        }
+        else{
+            echo "The book data is not available in the database";
+        }
+
+     }
+?>
